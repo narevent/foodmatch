0da86e0eded3d571af7fe8b1b7f2339c7d61d509
narevent <mr.rhythm@live.nl>
2026-02-02 03:56:09 +0100

    redirect to login

--- foodmatch/urls.py
+++ foodmatch/urls.py
@@ -2,10 +2,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from dating.views import login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('', lambda request: redirect('dating:discover')),
-    path('', admin.site.urls),
+    path('', login_view),
     path('dating/', include('dating.urls')),
 ]
